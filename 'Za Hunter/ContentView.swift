fed24cd8778987551dc518ed55dc459b115965d4
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-02-23 10:50:21 -0600

    Initial Commit

---
+++ 'Za Hunter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  'Za Hunter
+//
+//  Created by Nicholas Papierniak on 2/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Nicholas Papierniak on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
