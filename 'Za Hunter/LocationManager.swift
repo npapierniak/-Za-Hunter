f4dcce9ee68224013d9999a1c28d1110ebb07726
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-02-23 11:00:07 -0600

    Added user location

---
+++ 'Za Hunter/LocationManager.swift
@@ -0,0 +1,20 @@
+//
+//  LocationManager.swift
+//  'Za Hunter
+//
+//  Created by Nicholas Papierniak on 2/23/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+            super.init()
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+}
+
